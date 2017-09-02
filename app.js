34ee277aba38eaa16a867985e62391f383674726
Jake Tripp <shmogens@gmail.com>
2017-09-02 15:50:26 -0500

    finished

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 var sequence = [];
 var userSequence;
 var round = 1;
-var numberToWin = 2;
+var numberToWin = 20;
 
 var key = {
 	1: '#red',
@@ -37,14 +37,44 @@ function init(){
 	animateSequence();
 }
 
+function generateSequence(){
+	sequence = [];
+	userSequence = [];
+	for (var i = 1; i <= numberToWin; i++){
+		var curr = getRandomInt(1,4);
+		sequence.push(key[curr]);
+	}
+}
+
+function animateSequence(){
+	removeEventHandlers();
+
+	var i = 0;
+	// amount of milliseconds to end the setInterval
+	var timeToKillInterval = (round * 1000) + 500
+
+	var endIntervalID = setInterval(function(){
+		playAudio(sequence[i]);
+		$(sequence[i]).addClass('picked');
+		$(sequence[i]).on('transitionend', removeTransition);
+		i++;		
+	}, 1000);
+
+	// after animation, end the setInterval, add click events, track user
+	setTimeout(function(){
+		clearInterval(endIntervalID);
+		trackUserSequence();
+	},timeToKillInterval);	
+}
+
+
+// EVENTS
 function clickEvents(){
 	// animation and audio for tiles
 	$('.tile').on('click', function(e){
-
 		var id = '#' + e.target.id;
 		playAudio(id);
 		$(id).addClass('picked');		
-		
 	});
 	$('.buttons').on('transitionend', '.tile', removeTransition);
 
@@ -57,7 +87,7 @@ function clickEvents(){
 	keyDownEvent();
 }
 
-// just for fun, doesn't work yet
+// just for fun, doesn't get tracked though (kinda an Easter Egg)
 function keyDownEvent(){
 
 	var keyCodeToColor = {
@@ -78,18 +108,19 @@ function keyDownEvent(){
 	$('.buttons').on('transitionend', '.tile', removeTransition);
 }
 
+// updates the round number
 function updateDOM(){
-	$('label#round')[0].textContent = minTwoDigits(round);
+	$('label#round')[0].textContent = 'ROUND ' + minTwoDigits(round);
 	if (round === numberToWin){
 		$('label#round')[0].textContent = 'FINAL ROUND';
 	}
 }
 
+// RESETTING
 function reset(){
 	round = 1;
 	updateDOM();
 	generateSequence();
-	console.log(userSequence);
 }
 
 function removeTransition(e) {
@@ -107,40 +138,11 @@ function removeEventHandlers(){
 	$('body').off();
 }
 
+// plays audio (allows to play again before finishing completely)
 function playAudio(id){
 	var audio = $(id + ' audio')[0];
 	audio.currentTime = 0;
 	audio.play();
-}
-
-function generateSequence(){
-	sequence = [];
-	userSequence = [];
-	for (var i = 1; i <= numberToWin; i++){
-		var curr = getRandomInt(1,4);
-		sequence.push(key[curr]);
-	}
-	console.log(sequence);
-}
-
-function animateSequence(){
-	removeEventHandlers();
-
-	var i = 0;
-	var timeToKillInterval = (round * 1000) + 500
-
-	var endIntervalID = setInterval(function(){
-		playAudio(sequence[i]);
-		$(sequence[i]).addClass('picked');
-		$(sequence[i]).on('transitionend', removeTransition);
-		i++;		
-	}, 1000);
-
-	setTimeout(function(){
-		clearInterval(endIntervalID);
-		clickEvents();
-		trackUserSequence();
-	},timeToKillInterval);	
 }
 
 function trackUserSequence(){
@@ -155,52 +157,80 @@ function trackUserSequence(){
 			userSequence.push(id);
 		} 
 
-		console.log(userSequence)
 		outcomeOfUserSequence();
 	});
 }
 
+// checks if user presses a wrong tile
+function checkUserSequence(){
+
+	// the sequence so far
+	var miniSequence = sequence.slice(0, round);
+
+	// if the user doesn't match the sequence so far, return false
+    for(var i = 0; i < userSequence.length; i++) {
+        if(miniSequence[i] !== userSequence[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function outcomeOfUserSequence(){
-	// if strict mode, generate new sequence and reset round back to 1
+	// user pressed wrong tile
 	if (!checkUserSequence()) {
 
-		var strictModeEnabled = $('input[type="checkbox"]').prop('checked');
-		if (strictModeEnabled){
-			setTimeout(function(){
-				$('#fail')[0].play();
-			}, 1000);
-			reset();
-			setTimeout(function(){
-				animateSequence();
-			}, 7000)
-			return;
-		}
+		// with strict mode
+		strictModeAndLost();
+
 		setTimeout(function(){
 			animateSequence();
 		}, 2000)
 
+	// user pressed right tile
 	} else {
-		console.log('user sequence length ' + userSequence.length);
-		console.log('round ' + round);
-
-		// final sequence done
-		if (userSequence.length === numberToWin){
-			setTimeout(function(){
-				winMessage();
-			}, 300);
-			return;
-		}
-		// did correct sequence so far
-		if (userSequence.length === round){
-			round++;
-			updateDOM();
-			setTimeout(function(){
-				animateSequence();
-			}, 1000);
-		}
+		ifUserWon();
+		ifUserCompletedRound();
 	}
 }
 
+// play the trumpet sound, reset stuff, animate new sequence
+function strictModeAndLost(){
+	var strictModeEnabled = $('input[type="checkbox"]').prop('checked');
+	if (strictModeEnabled){
+		setTimeout(function(){
+			$('#fail')[0].play();
+		}, 1000);
+		reset();
+		setTimeout(function(){
+			animateSequence();
+		}, 7000)
+		return;
+	}
+}
+
+function ifUserWon(){
+	// final sequence done
+	if (userSequence.length === numberToWin){
+		setTimeout(function(){
+			winMessage();
+		}, 300);
+		return;
+	}
+}
+
+function ifUserCompletedRound(){
+	// did correct sequence so far
+	if (userSequence.length === round){
+		round++;
+		updateDOM();
+		setTimeout(function(){
+			animateSequence();
+		}, 1000);
+	}
+}
+
+// congrats sound, reset, and semantic modal for if user won
 function winMessage(){
 	$('#congrats')[0].play();
 	reset();
@@ -215,18 +245,6 @@ function winMessage(){
 			}
 		})
 		.modal('show');
-}
-
-function checkUserSequence(){
-
-	var miniSequence = sequence.slice(0, round);
-
-    for(var i = 0; i < userSequence.length; i++) {
-        if(miniSequence[i] !== userSequence[i]) {
-            return false;
-        }
-    }
-    return true;
 }
 
 init();
